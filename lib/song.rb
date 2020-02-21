d4b197f6b5edf0f0ca021d9ecfa1f41f249403c7
michael otero <michaelotero1118@gmail.com>
2020-02-21 01:03:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,44 @@ class Song
 
   def save
     self.class.all << self
+  end 
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song 
+  end 
+  
+  def self.new_by_name(title)
+    song = self.new 
+    song.name = title
+    song 
   end
+  
+  def self.create_by_name(title)
+    song = self.create 
+    song.name = title
+    song 
+  end
+  
+ def self.find_by_name(title)
+    result = self.all.detect { |song| song.name == title}
+    result
+  end
+  
+  def self.find_or_create_by_name(title)
+    result = self.find_by_name(title)
+    if result
+      result
+    else 
+      self.create_by_name(title)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end 
+  
+
 
 end
